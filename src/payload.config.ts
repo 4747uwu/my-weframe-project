2f19b8d5fba8ce7b6a0584c2446417cd3e018512
Abhishek Shukla <4747uwu@gmail.com>
2025-05-25 23:41:35 +0530

    pujs

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,8 @@ import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { formBuilder } from '@payloadcms/plugin-form-builder'
+
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
@@ -34,6 +36,16 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
+    formBuilder({
+      // Optionally redirect to a success page
+      // redirectUrl: '/contact-success',
+      // Optionally define fields that should be excluded from the form builder
+      // fields: {
+      //   // payment: false, // Example: disable payment field if you don't need it
+      // },
+      formOverrides: {}, // If you need to override the Forms collection itself
+      formSubmissionOverrides: {}, // If you need to override the Submissions collection
+    }),
     // storage-adapter-placeholder
   ],
 })
